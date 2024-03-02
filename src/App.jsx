daef531412c26911a12239930d3356c45cc4920e
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-02 20:31:05 +0200

    Introduction to props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import Header from "./Header";
+import componentsImg from "./assets/components.png";
+import CoreConcepts from "./CoreConcepts";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcepts
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcepts
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcepts
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
