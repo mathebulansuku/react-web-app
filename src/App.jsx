966680eb3a30758537edfb20f3cca36f6702109b
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-04 06:42:29 +0200

    map attribute added to the core concept section

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((items) => (
+              <CoreConcepts {...items} />
+            ))}
           </ul>
         </section>
         <section id="examples">
