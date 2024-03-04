af6899f0d354bfb93c97a923aace46b8de8f8b3d
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-04 03:52:48 +0200

    useState added to EXAMPLES section

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,11 @@ import TabButton from "./components/TabButton.jsx";
 import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
-  setSelectedTopic(selected);
-
-  function handleSelect(selected) {
-    console.log(selected);
+  function handleSelect(selectedButton) {
+    (selectedButton) => "components", "jsx", "props", "state";
+    return setSelectedTopic(selectedButton);
   }
 
   return (
@@ -31,18 +30,18 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("Components")}>
+            <TabButton onSelect={() => handleSelect("components")}>
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
           <div id="tab-content">
-            <h3></h3>
-            <p></p>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
-              <code></code>
+              <code>{EXAMPLES[selectedTopic].code}</code>
             </pre>
           </div>
         </section>
