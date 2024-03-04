14396fe93d3106dfcc8eb0295ba21c521d395a29
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-04 03:26:20 +0200

    new data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx";
 import componentsImg from "./assets/components.png";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("");
+
+  setSelectedTopic(selected);
+
   function handleSelect(selected) {
     console.log(selected);
   }
@@ -32,6 +38,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3></h3>
+            <p></p>
+            <pre>
+              <code></code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
